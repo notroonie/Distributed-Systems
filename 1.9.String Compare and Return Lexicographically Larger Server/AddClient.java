d43b869a78e4abbc2aec01ebee8b917ad3ca114c
notroonie <104883242+notroonie@users.noreply.github.com>
2024-04-26 00:18:06 +0530

    Add files via upload

---
+++ 1.9.String Compare and Return Lexicographically Larger Server/AddClient.java
@@ -0,0 +1,25 @@
+import java.rmi.*;
+public class AddClient {
+public static void main(String args[]) { 
+try {
+// Get reference to the remote object
+String addServerURL = "rmi://" + args[0] + "/AddServer"; 
+AddServerIntf addServerIntf =
+(AddServerIntf) Naming.lookup(addServerURL);
+System.out.println("First String is: " + args[1]); 
+String d1 =(args[1]);
+System.out.println("Second String is: " + args[2]); 
+String d2 =(args[2]);
+System.out.println("Greater String is : " + addServerIntf.scmp(d1,d2));
+} 
+catch (Exception e) { System.out.println("Exception: "+ e);
+}}}
+
+
+// Terminal 1 
+// javac *.java 
+// rmiregistry
+// Terminal 2
+// java AddServer
+// Terminal 3 
+// java AddClient 127.0.0.1 hello good
